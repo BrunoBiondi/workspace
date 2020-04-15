1c52be27858f9df9bf38d5f13326b7637da546ae
Bruno Biondi <eu@brunobiondi.com.br>
2020-04-15 08:56:06 -0300

    VSCode script installer extensions

---
+++ vscode/extensions.sh
@@ -0,0 +1,16 @@
+code --install-extension dbaeumer.vscode-eslint &&
+code --install-extension dracula-theme.theme-dracula &&
+code --install-extension EditorConfig.EditorConfig &&
+code --install-extension EmadFanaian.adonisjs-snippets &&
+code --install-extension esbenp.prettier-vscode &&
+code --install-extension gerane.Theme-Dark-Dracula &&
+code --install-extension jpoissonnier.vscode-styled-components &&
+code --install-extension mikestead.dotenv &&
+code --install-extension ms-azuretools.vscode-docker &&
+code --install-extension naumovs.color-highlight &&
+code --install-extension onecentlin.laravel5-snippets &&
+code --install-extension PKief.material-icon-theme &&
+code --install-extension rocketseat.RocketseatReactJS &&
+code --install-extension rocketseat.RocketseatReactNative &&
+code --install-extension teeLang.vsprettier &&
+code --install-extension wix.vscode-import-cost
